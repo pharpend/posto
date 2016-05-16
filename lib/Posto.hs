d9a1e946a9645ff272b46975080470c3dd804c17
Peter Harpending <peter@harpending.org>
2016-05-15 18:29:04 -0600

    Add Posto.Config module.

--- lib/Posto.hs
+++ lib/Posto.hs
@@ -1,17 +1,6 @@
--- |Main Posto library
-module Posto where
+-- |This module just re-exports @Posto.*@ for convenience.
+module Posto
+       ( module Posto.Config
+       ) where
 
-import Graphics.Vty
-
--- |Mostly copied from <the VTY example
--- https://hackage.haskell.org/package/vty-5.5.0/docs/Graphics-Vty.html>
-run :: IO ()
-run =
-  do cfg <- standardIOConfig
-     vty <- mkVty cfg
-     update vty helloPic
-     e <- (nextEvent vty)
-     shutdown vty
-     print e
-  where helloPic = picForImage helloImg
-        helloImg = text defAttr "hello, world"
+import Posto.Config as Posto.Config
